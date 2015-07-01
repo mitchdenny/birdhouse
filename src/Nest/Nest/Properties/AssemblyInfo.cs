0e27523fc88275e25ddb41428c64a16cfee14db5
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 01:05:00 +1000

    Added script for build stamping (step 1).

--- src/Nest/Nest/Properties/AssemblyInfo.cs
+++ src/Nest/Nest/Properties/AssemblyInfo.cs
@@ -9,23 +9,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Nest")]
 [assembly: AssemblyDescription("A .NET portable class library which provides easy access to Nest thermostats and smoke detectors via their REST API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nest")]
 [assembly: AssemblyProduct("Nest")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json")]
+[assembly: AssemblyVersion("0.9.0.0")]
