d75a29cccddda4f9fbea1337319f43cef61907da
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 01:29:53 +1000

    Fixed file locking issue (hopefully).

--- src/Nest/Nest/StampBuildVersions.ps1
+++ src/Nest/Nest/StampBuildVersions.ps1
@@ -2,5 +2,8 @@ param(
 	[string]$revision
 )
 
-Get-Content src\Nest\Nest\Properties\AssemblyInfo.cs | % { $_.Replace("0.9.0.0", "$revision"); } | Set-Content src\Nest\Nest\Properties\AssemblyInfo.cs
-Get-Content src\Nest\Nest\Nest.nuspec | % { $_.Replace("0.9.0.0", "$revision"); } | Set-Content src\Nest\Nest\Nest.nuspec
+$assemblyinfo = Get-Content src\Nest\Nest\Properties\AssemblyInfo.cs
+$assemblyinfo.Replace("0.9.0.0", "$revision") | Set-Content src\Nest\Nest\Properties\AssemblyInfo.cs
+
+$nuspec = Get-Content src\Nest\Nest\Nest.nuspec
+$nuspec.Replace("0.9.0.0", "$revision") | Set-Content src\Nest\Nest\Nest.nuspec
