e115b92f0d29178bfb7c4e13b5278c280d3836c5
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 01:25:45 +1000

    See if this build stamping script works OK.

--- src/Nest/Nest/StampBuildVersions.ps1
+++ src/Nest/Nest/StampBuildVersions.ps1
@@ -2,5 +2,5 @@ param(
 	[string]$revision
 )
 
-
-Write-Output "Stamping $revision" 
+Get-Content Properties\AssemblyInfo.cs | % { $_.Replace("0.9.0.0", "$revision"); } | Set-Content Properties\AssemblyInfo.cs
+Get-Content Nest.nuspec | % { $_.Replace("0.9.0.0", "$revision"); } | Set-Content Nest.nuspec
