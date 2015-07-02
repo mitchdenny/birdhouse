1af8c93aecaa81826f972bdb1c55b0f0666813d1
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 14:55:13 +1000

    Adding more logging.

--- src/Birdhouse/Birdhouse/StampBuildVersions.ps1
+++ src/Birdhouse/Birdhouse/StampBuildVersions.ps1
@@ -3,20 +3,30 @@ param(
 	[string]$buildmetadata
 )
 
+Write-Output "BuildNumber was: $buildnumber"
+Write-Output "BuildMetadata was: $buildmetadata"
+
 $semanticversion = $buildnumber.Split("_")[1];
 
+Write-Output "Semantic Version was: $semanticversion"
+
 $assemblyversion = $semanticversion.Replace("-build",".")
+
+Write-Output "Assembly Version was: $assemblyversion"
+
 $assemblyinfo = Get-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
 $assemblyinfo.Replace("0.9.0.0", "$assemblyversion") | Set-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
 
 if ($buildmetadata -eq "") {
-	$nuspecversion = $semanticversion.Replace("build", $buildmetadata)
-}
-else {
+	Write-Output "No BuildMetadata Detected!"
 	$nuspecversion = $semanticversion.Split("-")[0];
 }
+else {
+	Write-Output "BuildMetadata Detected!"
+    $nuspecversion = $semanticversion.Replace("build", $buildmetadata)
+}
 
-Write-Output "Nuspec version will be: $nuspecversion"
+Write-Output "Nuspec Version was: $nuspecversion"
 
 $nuspec = Get-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
 $nuspec.Replace("0.9.0-developer", "$nuspecversion") | Set-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
