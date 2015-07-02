dccb4d1a65fb23801a4514caa1613b13f4fafd5f
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 11:53:59 +1000

    Fixed up typo in stamping script.

--- src/Birdhouse/Birdhouse/StampBuildVersions.ps1
+++ src/Birdhouse/Birdhouse/StampBuildVersions.ps1
@@ -7,5 +7,5 @@ $version = $buildnumber.Split("_")[1];
 $assemblyinfo = Get-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
 $assemblyinfo.Replace("0.9.0.0", "$version") | Set-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
 
-$nuspec = Get-Content src\Nest\Nest\Nest.nuspec
+$nuspec = Get-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
 $nuspec.Replace("0.9.0.0", "$version") | Set-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
