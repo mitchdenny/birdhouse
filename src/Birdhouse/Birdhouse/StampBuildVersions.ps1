e4a1ab51c613c4037729248a5df3ee4f93e5bfac
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 11:49:11 +1000

    Replaced missing build stamper script.

---
+++ src/Birdhouse/Birdhouse/StampBuildVersions.ps1
@@ -0,0 +1,11 @@
+param(
+	[string]$buildnumber
+)
+
+$version = $buildnumber.Split("_")[1];
+
+$assemblyinfo = Get-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
+$assemblyinfo.Replace("0.9.0.0", "$version") | Set-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
+
+$nuspec = Get-Content src\Nest\Nest\Nest.nuspec
+$nuspec.Replace("0.9.0.0", "$version") | Set-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
