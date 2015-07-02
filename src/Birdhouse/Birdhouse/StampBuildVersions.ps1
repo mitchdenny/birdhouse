f215ecd57a1ebca55392f3d4f4832b6a68976891
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 12:17:16 +1000

    Working through semantic versioning issues and incompatibilities between SemVer and .NET versioning.

--- src/Birdhouse/Birdhouse/StampBuildVersions.ps1
+++ src/Birdhouse/Birdhouse/StampBuildVersions.ps1
@@ -1,11 +1,20 @@
 param(
-	[string]$buildnumber
+	[string]$buildnumber,
+	[bool]$ispreview
 )
 
-$version = $buildnumber.Split("_")[1];
+$semanticversion = $buildnumber.Split("_")[1];
 
+$assemblyversion = $semanticversion.Replace("-build",".")
 $assemblyinfo = Get-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
-$assemblyinfo.Replace("0.9.0.0", "$version") | Set-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
+$assemblyinfo.Replace("0.9.0.0", "$assemblyversion") | Set-Content src\Birdhouse\Birdhouse\Properties\AssemblyInfo.cs
+
+if ($ispreview) {
+	$nuspecversion = $semanticversion.Split("-")[0];
+}
+else {
+	$nuspecversion = $semanticversion
+}
 
 $nuspec = Get-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
-$nuspec.Replace("0.9.0.0", "$version") | Set-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
+$nuspec.Replace("0.9.0-developer", "$nuspecversion") | Set-Content src\Birdhouse\Birdhouse\Birdhouse.nuspec
