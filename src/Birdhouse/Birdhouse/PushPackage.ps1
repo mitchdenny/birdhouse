d979f0ada830010134514d9f577aaed18e674d93
Mitch Denny <mitch@mitchdenny.com>
2015-07-02 15:05:31 +1000

    Updated publishing script paths.

--- src/Birdhouse/Birdhouse/PushPackage.ps1
+++ src/Birdhouse/Birdhouse/PushPackage.ps1
@@ -2,4 +2,4 @@ param(
 	[string]$apikey
 )
 
-src\Nest\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe push src\Nest\Nest\bin\Release\*.nupkg $apikey
+src\Birdhouse\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe push src\Birdhouse\Birdhouse\bin\Release\*.nupkg $apikey
